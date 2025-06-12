c2ae4d0dd1af7e3fb04ddfc2f8b63fc34ebe8d6c
sheida-shab <shabankary@gmail.com>
2025-06-12 17:15:14 +0100

    Update 0.js
    fixing the error of multiply function

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,7 +1,8 @@
 // Predict and explain first...
 
 // =============> write your prediction here
-
+The function should return multiply of two input parameters(a and b) but it returns error .
+  
 function multiply(a, b) {
   console.log(a * b);
 }
@@ -9,6 +10,12 @@ function multiply(a, b) {
 console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
-
+Because in the body of function we don't have "return" for output result.we shouldn't use console.log() for printing ther esult of a function.
+  
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);

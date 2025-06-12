3707ddfc954debd107d527e02b5cd48f34eca0c5
sheida-shab <shabankary@gmail.com>
2025-06-12 17:21:04 +0100

    Update 1.js
    fixing the function sum error

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
 //  =============> write your prediction here
+output is : The sum of 10 and 32 is undefined
 
 function sum(a, b) {
   return;
@@ -9,5 +10,11 @@ function sum(a, b) {
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
 // =============> write your explanation here
+In this function ,the first statement is return and it ends without any action or result.calling this function returns undefined value.
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+  function sum(a, b) {
+  return  a + b;
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);

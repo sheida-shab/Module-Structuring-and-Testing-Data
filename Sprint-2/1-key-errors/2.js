1a4752cfee02e092e8d3f9f3db42c75ea80efcc9
sheida-shab <shabankary@gmail.com>
2025-06-12 17:00:04 +0100

    Update 2.js
    fixing function declaring error

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -4,17 +4,22 @@
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
+undefined variable num is not declared
 
 function square(3) {
     return num * num;
 }
 
 // =============> write the error message here
-
+SyntaxError: Unexpected number
+SyntaxError: Unexpected token
 // =============> explain this error message here
-
+The compiler expected a parameter name in declaring the function but it found a number instead
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+    return num * num;
+}
+square(3);

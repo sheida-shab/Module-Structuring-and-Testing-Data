76030d4869890474b5594f004cfbbcd0b24c8949
sheida-shab <shabankary@gmail.com>
2025-06-12 14:02:35 +0100

    Update 1.js
    find and fix the error

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -2,7 +2,7 @@
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
-
+decimalNumber can not be printed by console.log because it isn't string type .
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
@@ -15,6 +15,14 @@ function convertToPercentage(decimalNumber) {
 console.log(decimalNumber);
 
 // =============> write your explanation here
-
+error: SyntaxError: Identifier 'decimalNumber' has already been declared. 
+decimalNumber is declared as function's parameter and again as const variable.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));

958467fce6446ce465e4b1d4c59737d7f3364042
sheida-shab <shabankary@gmail.com>
2025-06-12 19:45:30 +0100

    Update time-format.js
    play computer time-format

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -18,17 +18,25 @@ function formatTimeDisplay(seconds) {
 
 // a) When formatTimeDisplay is called how many times will pad be called?
 // =============> write your answer here
+3 times : pad(totalHours) , pad(remainingMinutes) , pad(remainingSeconds)
 
 // Call formatTimeDisplay with an input of 61, now answer the following:
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
-
+0
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
-
+00
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+1
+The last call of pad is pad(remainingSeconds).
+remainingSeconds is calculated by remainder after dividing seconds variable value  by 60 . 
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
+01
+function pad : 
+first change num type to string
+second add 0 at the beginning of num until the length becomes 2.

71fa8a29b9c45725cc712fe5c9975b99aed179b7
sheida-shab <shabankary@gmail.com>
2025-06-14 16:05:41 +0100

    writing and passing test cases for angels

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.test.js
@@ -10,15 +10,26 @@ test("should identify right angle (90°)", () => {
 // Case 2: Identify Acute Angles:
 // When the angle is less than 90 degrees,
 // Then the function should return "Acute angle"
+test("should identify Acute angle less than (90°)", () => {
+  expect(getAngleType(89.99)).toEqual("Acute angle");
+});
 
 // Case 3: Identify Obtuse Angles:
 // When the angle is greater than 90 degrees and less than 180 degrees,
 // Then the function should return "Obtuse angle"
+test("should identify Obtuse angle greater than (90°) and less than (180°)", () => {
+  expect(getAngleType(120)).toEqual("Obtuse angle");
+});
 
 // Case 4: Identify Straight Angles:
 // When the angle is exactly 180 degrees,
 // Then the function should return "Straight angle"
-
+test("should identify Straight angle  (180°)", () => {
+  expect(getAngleType(180)).toEqual("Straight angle");
+});
 // Case 5: Identify Reflex Angles:
 // When the angle is greater than 180 degrees and less than 360 degrees,
-// Then the function should return "Reflex angle"
+// Then the function should return "node 1 angle"
+test("should identify node 1 angle greater than (180°)  and less than (360°)", () => {
+  expect(getAngleType(220)).toEqual("node 1 angle");
+});

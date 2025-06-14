89c7448383c85b13d7dc7c82909f14516c4e4b6c
sheida-shab <shabankary@gmail.com>
2025-06-14 16:07:18 +0100

    implementing function for proper fraction

--- Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
+++ Sprint-3/2-mandatory-rewrite/2-is-proper-fraction.js
@@ -1,6 +1,9 @@
 function isProperFraction(numerator, denominator) {
-    if (numerator < denominator) return true;
-    // add your completed function from key-implement here
+  if (numerator < denominator && numerator > 0 && denominator > 0) return true;
+  // add your completed function from key-implement here
+  if (numerator > denominator) return false;
+  if (numerator < 0 || denominator < 0) return false;
+  if (numerator === denominator) return false;
 }
 
 module.exports = isProperFraction;

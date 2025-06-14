d9a58aaf6906a1e016865cd6611edd95c6ef2055
sheida-shab <shabankary@gmail.com>
2025-06-14 16:10:34 +0100

    implementing function for card values

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,5 +1,16 @@
 function getCardValue(card) {
-    // replace with your code from key-implement
+  // replace with your code from key-implement
+  if (card === "A♠") {
     return 11;
+  }
+  if (card >= 2 || card <= 10) {
+    return "2 - 9";
+  }
+  if (["J", "Q", "K"].includes(card)) {
+    return "Face Card";
+  }
+  if (card[0] === "A") {
+    return "Ace Card";
+  }
 }
 module.exports = getCardValue;

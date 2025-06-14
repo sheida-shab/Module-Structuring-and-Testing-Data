44ed00b04bfd9ad4190629570f6e25e048b068b0
sheida-shab <shabankary@gmail.com>
2025-06-14 16:11:35 +0100

    writing and passing test cases for  function  card values

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,25 @@
 const getCardValue = require("./3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
-    });
+  const aceofSpades = getCardValue("A♠");
+  expect(aceofSpades).toEqual(11);
+});
 
 // Case 2: Handle Number Cards (2-10):
+test("should return 2-9 for 2", () => {
+  const numberCards = getCardValue("2");
+  expect(numberCards).toEqual("2 - 9");
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return Face Card for J", () => {
+  const faceCards = getCardValue("J");
+  expect(faceCards).toEqual("Face Card");
+});
+
 // Case 4: Handle Ace (A):
+test("should return Ace for A", () => {
+  const aceCards = getCardValue("A");
+  expect(aceCards).toEqual("Ace Card");
+});
 // Case 5: Handle Invalid Cards:

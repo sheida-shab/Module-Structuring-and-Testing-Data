0cd41c088e43296a257a50fb571e475c09289bc1
sheida-shab <shabankary@gmail.com>
2025-06-16 12:55:37 +0100

    testing and implementing getOrdinalNumber  function

--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.js
@@ -1,5 +1,23 @@
 function getOrdinalNumber(num) {
- return "1st";
+  if ([11, 12, 13].includes(num)) {
+    return num.toString() + "th";
+  }
+  let lastDigit = num.toString().slice(-1);
+  if (lastDigit === "1") {
+    return num.toString() + "st";
+  }
+  if (lastDigit === "0") {
+    return num.toString() + "th";
+  }
+  if (lastDigit === "2") {
+    return num.toString() + "nd";
+  }
+  if (lastDigit === "3") {
+    return num.toString() + "rd";
+  }
+  if (["4", "5", "6", "7", "8", "9"].includes(lastDigit)) {
+    return num.toString() + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
--- Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
+++ Sprint-3/3-mandatory-practice/implement/get-ordinal-number.test.js
@@ -9,5 +9,44 @@ const getOrdinalNumber = require("./get-ordinal-number");
 // Then the function should return "1st"
 
 test("should return '1st' for 1", () => {
-    expect(getOrdinalNumber(1)).toEqual("1st");
-    });
+  expect(getOrdinalNumber(1)).toEqual("1st");
+});
+test("should return '21st' for 21", () => {
+  expect(getOrdinalNumber(21)).toEqual("21st");
+});
+test("should return '11th' for 11", () => {
+  expect(getOrdinalNumber(11)).toEqual("11th");
+});
+test("should return '2nd' for 2", () => {
+  expect(getOrdinalNumber(2)).toEqual("2nd");
+});
+test("should return '32nd' for 32", () => {
+  expect(getOrdinalNumber(32)).toEqual("32nd");
+});
+test("should return '12th' for 12", () => {
+  expect(getOrdinalNumber(12)).toEqual("12th");
+});
+test("should return '13th' for 13", () => {
+  expect(getOrdinalNumber(13)).toEqual("13th");
+});
+test("should return '3rd' for 3", () => {
+  expect(getOrdinalNumber(3)).toEqual("3rd");
+});
+test("should return '23rd' for 23", () => {
+  expect(getOrdinalNumber(23)).toEqual("23rd");
+});
+test("should return '4th' for 4", () => {
+  expect(getOrdinalNumber(4)).toEqual("4th");
+});
+test("should return '24th' for 24", () => {
+  expect(getOrdinalNumber(24)).toEqual("24th");
+});
+test("should return '103rd' for 103", () => {
+  expect(getOrdinalNumber(103)).toEqual("103rd");
+});
+test("should return '100th' for 100", () => {
+  expect(getOrdinalNumber(100)).toEqual("100th");
+});
+test("should return '10th' for 10", () => {
+  expect(getOrdinalNumber(10)).toEqual("10th");
+});

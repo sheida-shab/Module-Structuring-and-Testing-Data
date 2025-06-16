b3c1079ae39888f55665b68296b65a03ab1c3db5
sheida-shab <shabankary@gmail.com>
2025-06-16 12:06:04 +0100

    implement and test  function countChar

--- Sprint-3/3-mandatory-practice/implement/count.test.js
+++ Sprint-3/3-mandatory-practice/implement/count.test.js
@@ -16,9 +16,26 @@ test("should count multiple occurrences of a character", () => {
   const count = countChar(str, char);
   expect(count).toEqual(5);
 });
-
+test("should count multiple occurrences of a character", () => {
+  const str = "absfka";
+  const char = "a";
+  const count = countChar(str, char);
+  expect(count).toEqual(2);
+});
 // Scenario: No Occurrences
 // Given the input string str,
 // And a character char that does not exist within the case-sensitive str,
 // When the function is called with these inputs,
 // Then it should return 0, indicating that no occurrences of the char were found in the case-sensitive str.
+test("should count 0 for no occurrences of a character", () => {
+  const str = "absfka";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});
+test("should count 0 for no occurrences of a character & case sensitive ", () => {
+  const str = "Zbsfka";
+  const char = "z";
+  const count = countChar(str, char);
+  expect(count).toEqual(0);
+});

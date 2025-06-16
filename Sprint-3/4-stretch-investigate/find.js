7f28d1af90a0ed6c71e8e921164f40462719eb00
sheida-shab <shabankary@gmail.com>
2025-06-16 19:25:58 +0100

    play computer find function

--- Sprint-3/4-stretch-investigate/find.js
+++ Sprint-3/4-stretch-investigate/find.js
@@ -20,6 +20,14 @@ console.log(find("code your future", "z"));
 // Pay particular attention to the following:
 
 // a) How the index variable updates during the call to find
+//1- declare and assign let index = 0;
+//2-compare with  str.length and if it was less than str.length  assign index=index+1
+
 // b) What is the if statement used to check
+// It checks either the value of input parameter (char) is equal to the value of index character of the str input parameter.
+
 // c) Why is index++ being used?
+//To check all the characters in str one by one
+
 // d) What is the condition index < str.length used for?
+//To go through and check the last character of str

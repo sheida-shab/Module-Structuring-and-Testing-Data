1c6712ca4f06ef3829621d38174db75ad0fb690f
sheida-shab <shabankary@gmail.com>
2025-06-18 19:52:03 +0100

    add some comments

--- Sprint-3/4-stretch-investigate/password-validator.js
+++ Sprint-3/4-stretch-investigate/password-validator.js
@@ -1,8 +1,13 @@
 function passwordValidator(password) {
+  //password has at least 5 characters
   if (password.length >= 5) {
+    //password has at least one English uppercase letter (a-z)
     if (containUpperCase(password)) {
+      //password has at least one English lowercase letter (a-z)
       if (containLowerCase(password)) {
+        //password has at least one of the following non-alphanumeric symbols: (!, #, $, %, ., *, &)
         if (containSymbols(password)) {
+          //password Must not be any previous password in the passwords array
           if (checkPreviousPass(password)) {
             return true;
           }

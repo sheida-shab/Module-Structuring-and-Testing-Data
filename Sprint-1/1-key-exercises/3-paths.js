d0593ec69238109aed181ba95b1bd5b8f4466b90
sheida-shab <shabankary@gmail.com>
2025-06-20 10:16:05 +0100

    fix  the / issue in dir

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = filePath.slice(1,filePath.lastIndexOf("/"));
+const dir = filePath.slice(0,filePath.lastIndexOf("/"));
 const ext = filePath.slice(filePath.lastIndexOf(".")+1);
 
 

668d5749b79f503eaf28a9c23321338e21ac4bff
sheida-shab <shabankary@gmail.com>
2025-06-27 21:40:46 +0100

    Add double quote

--- Sprint-2/4-mandatory-interpret/time-format.js
+++ Sprint-2/4-mandatory-interpret/time-format.js
@@ -24,19 +24,19 @@ function formatTimeDisplay(seconds) {
 
 // b) What is the value assigned to num when pad is called for the first time?
 // =============> write your answer here
-0
+"0"
 // c) What is the return value of pad is called for the first time?
 // =============> write your answer here
-00
+"00"
 // d) What is the value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
-1
+"1"
 The last call of pad is pad(remainingSeconds).
 remainingSeconds is calculated by remainder after dividing seconds variable value  by 60 . 
 
 // e) What is the return value assigned to num when pad is called for the last time in this program?  Explain your answer
 // =============> write your answer here
-01
+"01"
 function pad : 
 first change num type to string
-second add 0 at the beginning of num until the length becomes 2.
+second add "0" at the beginning of num until the length becomes 2.

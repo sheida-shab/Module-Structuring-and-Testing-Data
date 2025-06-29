68cf38684a864cbaa40d0b920c5087d8adda0ce2
sheida-shab <shabankary@gmail.com>
2025-06-29 23:22:12 +0100

    improve the function checking

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.js
@@ -1,18 +1,18 @@
 function getCardValue(card) {
   // replace with your code from key-implement
   let rank = card[0];
-  if (card.length > 2) {
-    rank = card.slice(0, card.length - 1);
-  }
+  
+  if (["♠", "♥", "♦", "♣"].includes(card.slice(-1))){
+    if (card.length > 2) {
+      rank = card.slice(0, card.length - 1);
+    }
 
   if (rank === "A") return 11;
-  if (rank >= 2 && rank <= 10 && ["♠", "♥", "♦", "♣"].includes(card.slice(-1)))
+  if (["2", "3", "4", "5", "6", "7", "8", "9", "10"].includes(rank))
     return parseInt(rank);
-  if (
-    ["J", "Q", "K"].includes(rank) &&
-    ["♠", "♥", "♦", "♣"].includes(card.slice(-1))
-  )
+  if ( ["J", "Q", "K"].includes(rank) )
     return 10;
+}
   throw new Error("Invalid card rank.");
 }
 module.exports = getCardValue;
